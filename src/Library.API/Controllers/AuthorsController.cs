9d0bd7ad1042b6e67f45b283aade1cde035e9a6a
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-03-17 11:15:05 -0300

    Supporting the OPTIONS and HEAD methods

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -32,6 +32,7 @@ namespace Library.API.Controllers
         }
 
         [HttpGet(Name = "GetAuthors")]
+        [HttpHead]
         public IActionResult GetAuthors(AuthorsResourceParameters authorsResourceParameters,
                                         [FromHeader(Name = "Accept")] string mediaType)
         {
@@ -317,6 +318,13 @@ namespace Library.API.Controllers
             }
 
             return links;
+        }
+
+        [HttpOptions]
+        public IActionResult GetAuthorsOptions()
+        {
+            Response.Headers.Add("Allow", "GET,OPTIONS,POST");
+            return Ok();
         }
     }
 }
